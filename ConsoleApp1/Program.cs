6ea1d4e272fcc4ca2ccb0d36172506d35f8f3817
Pavlo Honcharevskyi <fix.pgoncharevskiy@gmail.com>
2023-12-26 08:46:01 +0200

    done

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using BookManagerProject;
+
+namespace BookLibrar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myBook = new Book("Harry Potter", "Joahn Rowling", "Fantasy"); var myBook2 = new Book("Harry Potter 2", "Joahn Rowling", "Fantasy");
+            Console.WriteLine($"{myBook.Title} by {myBook.Author}");
+            Console.WriteLine($"{myBook2.Title} by {myBook2.Author}");
+        }
+    }
+}
